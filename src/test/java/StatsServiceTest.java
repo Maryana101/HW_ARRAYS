dea59da943c99e6f128e2428a8724edd01970953
Maryana Varfolomeeva <mihralievam@icloud.com>
2023-08-22 11:34:44 +0500

    added assert for tests

--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.stats.StatsService;
 
@@ -8,36 +9,42 @@ public class StatsServiceTest {
         StatsService statsService = new StatsService();
         int expected=9;
         int actual= statsService.minSales(testData);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldReturnMaxSalesMonth(){
         StatsService statsService = new StatsService();
         int expected=8;
         int actual= statsService.maxSales(testData);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldReturnSumSales(){
         StatsService statsService = new StatsService();
         int expected=180;
         int actual= statsService.sumSales(testData);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldReturnAvgSales(){
         StatsService statsService = new StatsService();
         int expected=15;
         int actual= statsService.avgSales(testData);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldReturnAboveAvgSales(){
         StatsService statsService = new StatsService();
         int expected=5;
         int actual= statsService.aboveAvg(testData);
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void shouldReturnBelowAvgSales(){
         StatsService statsService = new StatsService();
         int expected=5;
         int actual= statsService.belowAvg(testData);
+        Assertions.assertEquals(expected, actual);
     }
 
 }
